6f5c96a5668797f2a89dd1df4840edcf4e3c131a
Michael Novac-Yu <michael.novacyu@teamtreehouse.com>
2018-02-24 12:27:33 -0800

    add flask exercises

---
+++ flask/4_passing_objects_templates.py
@@ -0,0 +1,15 @@
+# Exercise on passing objects (like a Python list) into templates
+# Ex: Passing through a shopping list
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/shopping")
+def shopping():
+    food = ["Cheese", "Tuna", "Beef", "Toothpaste"]
+    return render_template("shopping.html", food=food)
+
+
+if __name__ == "__main__":
+    app.run()
