cd1c2941d50d03a212482c94c92e1c9c6dead79a
mnovacyu <michael.novacyu@gmail.com>
2018-06-22 10:45:17 -0700

    improve drawing methods

--- practice_python/24_gameboard.py
+++ practice_python/24_gameboard.py
@@ -1,18 +1,15 @@
+# Simple board game drawing script
 def draw_horizontal(size):
-    for x in range(0, size):
-        print(" ---", end="")
-    print()
+    print(" ---" * size)
 
 def draw_vertical(size):
-    for x in range(0, size+1):
-        print("|   ", end="")
-    print()
+    print("|   " * (size+1))
 
 def draw_game_board(size):
     for x in range(0, size):
         draw_horizontal(size)
         draw_vertical(size)
-    draw_horizontal(size)
+    draw_horizontal(size) # close off the board
 
 while True:
     size = input("What sized game board would you like? ")
