5e9c66b13a59c7df6d0508c467248f12c6e1dab2
mnovacyu <michael.novacyu@gmail.com>
2018-09-08 12:11:05 -0700

    fix recursion

--- datastructures_algorithms/fibonacci.py
+++ datastructures_algorithms/fibonacci.py
@@ -1,11 +1,14 @@
 '''
 Utilize recursion to create a Fibonacci algorithm.
+fib(n) will give you the nth number in a Fibonacci sequence
+
+e.g. fib(6) = 8
 '''
 
 def fib(n):
-    while n >= 1:
-        return n + fib(n-1)
+    while n >= 3:
+        return fib(n-1) + fib(n-2)
     else:
-        return 0
+        return 1
 
-print(fib(100))
+print(fib(6))
